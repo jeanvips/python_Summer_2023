b594822d867be8d68a99bbfd92b4d591ef06fc85
user.name <jeanvips12345678@gmail.com>
2023-09-24 23:04:31 +0300

    Задача 3-3

--- TeachPython/lesson3/Task_3_3.py
+++ TeachPython/lesson3/Task_3_3.py
@@ -3,5 +3,12 @@ p = n.split(' ')
 for i in p:
     i.split()
 print(max(p, key=len))
-
-
+l = 0
+s = []
+for r in p:
+    if len(r) > l:
+        s = [r]
+        l = len(r)
+    elif len(r) == l:
+        s.append(r)
+print(s)
